c3792acfd3045bb27d9ef9a63af4accd487768be
nur-nayeem <nurnayem768@gmail.com>
2025-09-06 09:28:58 +0600

    added news container section

--- index.js
+++ index.js
@@ -20,10 +20,48 @@ loadCategories();
 const displayCategories = (categories) => {
     categories.forEach(categoy => {
         categoriesContainer.innerHTML += `
-        <li class="hover:border-b-4 border-red-600 cursor-pointer">${categoy.title}</li>
+        <li id="${categoy.id}" class="hover:border-b-4 border-red-600 cursor-pointer">${categoy.title}</li>
         `
+    })
+
+    categoriesContainer.addEventListener("click", (e) => {
+
+        if (e.target.localName === 'li') {
+            const allLi = document.querySelectorAll('li');
+            allLi.forEach(li => li.classList.remove("border-b-4"))
+            e.target.classList.add("border-b-4")
+            loadNewsByCategory(e.target.id)
+        }
+
     })
 
 }
 
 
+//news showing functionality
+
+const loadNewsByCategory = (categoryId) => {
+    fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
+        .then(res => res.json())
+        .then(news => {
+            displayNewsByCategory(news.articles);
+        })
+}
+
+const displayNewsByCategory = (articles) => {
+    newsContainer.innerHTML = ""
+    articles.forEach(article => {
+        newsContainer.innerHTML += `
+             <div class="border border-gray-100 p-2">
+                <img src='${article.image.srcset[5].url}' alt="news-img">
+                <h2 class="text-xl font-medium my-2">${article.title}}</h2>
+                <p class="font-light">${article.time}</p>
+            </div>
+        `
+        console.log(article);
+
+    })
+}
+
+
+loadNewsByCategory("main")
